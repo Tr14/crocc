f811895f4ce591fb5bf826f4d3bfd919b0d6a4ba
Tr14 <xuantrucx111@gmail.com>
2024-08-06 17:46:24 +0700

    first commit

--- start_game.js
+++ start_game.js
@@ -120,7 +120,9 @@ app.post('/updatestatus', async (req, res) => {
   for (let i = 0; i < req.body.data.length; i++) {
     let user_id = req.body.data[i].ID
     let user_status = req.body.data[i].STATUS
-    let user_desc = req.body.data[i].DESC 
+      let user_desc = req.body.data[i].DESC 
+      
+      console.log(user_desc)
 
     const inputValues = [user_id, user_status, user_desc];
 
