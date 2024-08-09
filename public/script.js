204806fc53fadf100119cdeb2cb5941fee3f928b
Tr14 <xuantrucx111@gmail.com>
2024-08-09 17:53:27 +0700

    4th commit

--- public/script.js
+++ public/script.js
@@ -195,6 +195,7 @@ button.addEventListener('click', function () {
                 .then((response) => response.json())
                 .then((result_post) => {
                     console.log(result_post)
+                    /*
                     if (result_post.message === 'FAIL_0' && result_post.list === "filteredArray_dif") {
                         document.getElementById('errorMessage').innerHTML = "Vui lòng không để trống thông tin"
                         document.getElementById('errorMessage').style.display = "block"
@@ -211,6 +212,7 @@ button.addEventListener('click', function () {
                     } else {
                         console.log(result_post)
                     }
+                    */
                 }
             ).catch((error) => console.error(error));
         }
